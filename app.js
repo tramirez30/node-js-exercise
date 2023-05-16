132adf171f0827dd1907d433b3c945b2faf99a52
tramirez30 <ramirez.tomas@yahoo.com>
2023-05-15 22:01:02 -0700

    setTimeout example

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const { readFile } = require("fs");
-
-console.log("started a first task");
-// Check File Path
-readFile("./content/first.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(result);
-  console.log("completed first task");
-});
-console.log("starting next task");
+// Started operating system process
+console.log("first");
+setTimeout(() => {
+  console.log("second");
+}, 0);
+console.log("third");
+// completed and exited operating system process
