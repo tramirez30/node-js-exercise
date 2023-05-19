ea2e62f41d5feb44987e673e49a80190d5ceae24
tramirez30 <ramirez.tomas@yahoo.com>
2023-05-19 16:51:48 -0700

    writing new files in nodejs

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/new-text-file.txt",
+      `This is a new file!: ${first} ${second}`,
+      { flag: "a" }
+    );
     console.log("Await Resolved:", first, second);
   } catch (error) {
     console.log("Error:", error);
@@ -27,3 +24,15 @@ start();
 // getText("./content/first.txt")
 //   .then((res) => console.log(res))
 //   .catch((err) => console.log(err));
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
